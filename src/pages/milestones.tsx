df4cfb1ff4fad91814ce072a179b9a95a4c0f101
Are <johnsonare2207@gmail.com>
2024-09-25 19:53:46 +0100

    milestone missing part

--- src/pages/milestones.tsx
+++ src/pages/milestones.tsx
@@ -3,6 +3,8 @@ import { useNavigate } from "react-router-dom";
 import Input from "../components/form/input";
 import Button from "../components/form/button";
 import FormGroup from "../components/form/formgroup";
+import { LuWatch } from "react-icons/lu";
+import { CiCircleCheck, CiMoneyBill } from "react-icons/ci";
 
 function Milestones() {
   const navigate = useNavigate();
@@ -12,6 +14,52 @@ function Milestones() {
         <p className="max-md:text-2xl lg:text-3xl font-extrabold max-md:text-center max-sm:tracking-tighter lg:tracking-tight">
           Set up milestones for invoice #INV-001
         </p>
+        <div className="flex flex-row justify-start w-full lg:w-[60%] my-4">
+          <p className="max-md:text-lg text-left lg:text-xl font-extrabold max-sm:tracking-tighter lg:tracking-tight">
+            Milestone 1:
+            <div className="flex flex-row mt-4 gap-4">
+              <div className="flex flex-col items-center gap-2">
+                <LuWatch size={25} />
+                <div className="p-0.5 bg-bg_light w-1 h-8"></div>
+              </div>
+              <div className="flex flex-col items-start gap-1">
+                <p className="text-sm text-left font-semibold text-text">
+                  Requested
+                </p>
+                <p className="text-xs text-left font-normal text-text_light">
+                  01/01/2023
+                </p>
+              </div>
+            </div>
+            <div className="flex flex-row mt-1 gap-4">
+              <div className="flex flex-col items-center gap-2">
+                <CiCircleCheck size={25} />
+                <div className="p-0.5 bg-bg_light w-1 h-8"></div>
+              </div>
+              <div className="flex flex-col items-start gap-1">
+                <p className="text-sm text-left font-semibold text-text">
+                  Approved
+                </p>
+                <p className="text-xs text-left font-normal text-text_light">
+                  01/01/2023
+                </p>
+              </div>
+            </div>{" "}
+            <div className="flex flex-row mt-1 gap-4">
+              <div className="flex flex-col items-center gap-1">
+                <CiMoneyBill size={25} />
+              </div>
+              <div className="flex flex-col items-start gap-1">
+                <p className="text-sm text-left font-semibold text-text">
+                  Paid
+                </p>
+                <p className="text-xs text-left font-normal text-text_light">
+                  01/01/2023
+                </p>
+              </div>
+            </div>
+          </p>
+        </div>
         <div className="max-md:w-full lg:w-[60%] flex flex-col mt-8">
           <Input
             type="textarea"
